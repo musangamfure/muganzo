81931bee644a4c0c5ef4eff4d9a425336ae82ca4
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-07-13 12:43:06 +0200

    Update menu

--- components/header.tsx
+++ components/header.tsx
@@ -11,9 +11,13 @@ const HeaderComponent = () => {
     setShowMenu((prev) => !prev);
   };
 
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <header className="flex justify-between items-center px-6 absolute z-10 w-full ">
-      <div className=" flex justify-between w-full items-center py-3 border-b-2 border-white">
+    <header className="flex justify-between items-center px-6 absolute z-10 w-full">
+      <div className="flex justify-between w-full items-center py-3 border-b-2 border-white">
         <Link href="/" className="font-bebas text-white text-xl">
           Muganzo
         </Link>
@@ -21,11 +25,13 @@ const HeaderComponent = () => {
         <ul
           className={`flex flex-col duration-300 sm:flex-row absolute right-0 bg-black w-screen h-screen sm:h-fit sm:w-fit sm:top-0 sm:relative sm:bg-transparent gap-6 justify-center items-center ${
             showMenu ? "top-0" : "-top-[100vh]"
-          } `}
+          }`}
         >
           {headerData.header.map((item, index) => (
             <li key={index} className="text-white text-base uppercase">
-              <Link href={item.href}>{item.label}</Link>
+              <Link href={item.href} onClick={closeMenu}>
+                {item.label}
+              </Link>
             </li>
           ))}
         </ul>
@@ -37,7 +43,7 @@ const HeaderComponent = () => {
             {showMenu ? "Close" : "Menu"}
           </button>
           <Link
-            href={"/contact"}
+            href="/contact"
             className="sm:block font-bebas hidden border-2 border-white rounded-full px-3 py-1 text-white hover:bg-white hover:text-black duration-300"
           >
             Contact Us
